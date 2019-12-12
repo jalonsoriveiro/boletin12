fc463cd8fd234a7136c2ca42707ebc04143998be
Jose Alonso <jalonsoriveiro@danielcastelao.org>
2019-12-12 13:50:02 +0100

    Primer boletin

---
+++ boletin12/Garaxe.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin12;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author jalonsoriveiro
+ */
+public class Garaxe {
+    
+    private int numeroCoches=0;
+    private String matricula;
+    private int plazasGaraxe=0;
+    public Garaxe(){                
+    }
+    public Garaxe(int p_numero_coches,String p_matricula){
+        
+        this.numeroCoches = p_numero_coches;
+        this.matricula = p_matricula;                
+    }
+
+    public int getNumeroCoches() {
+        return numeroCoches;
+    }
+
+    public void setNumeroCoches(int p_numeroCoches) {
+        
+        comprobarGaraxe(p_numeroCoches);
+        this.numeroCoches = numeroCoches;
+        
+        
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public int getPlazasGaraxe() {
+        return plazasGaraxe;
+    }
+
+    public void setPlazasGaraxe(int plazasGaraxe) {
+        this.plazasGaraxe = plazasGaraxe;
+    }
+    
+     // Antes de deixar aparcar un coche.
+     // Debemos comprobar que temos sitio para él,
+     //en caso afirmativo poñemos unha mensase “ PLAZAS DISPOÑIBLES “ 
+    //e rexistramos o coche . No caso contrario poríamos unha mensaxe “ 
+    //COMPLETO “.
+    public String comprobarGaraxe(int p_numeroCoches){
+       
+        System.out.println("Coche nuevo ");
+//return numero;
+        return null;
+        }
+    
+}
